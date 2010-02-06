96d9f8d5b5c6f0e8b0b4888373a442e93519c06d
Robert Stehwien <robert.stehwien@universalmind.com>
2010-02-05 19:28:00 -0700

    Test read of the xml data

---
+++ Resources/scripts/wtcc_app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$.ajax({
+		type: "GET",
+		url: "xml/WildTalentsCharacterCreatorData.xml",
+		dataType: "xml",
+		success: function(xml) {
+			$(xml).find('config-choice').each(function(){
+				var type = $(this).attr('type');
+				var link = $(this).attr('link');
+				$('<div class="items"></div>').html('<p>'+type+': '+link+'</a>').appendTo('#page-wrap');
+			});
+		}
+	});
+});
