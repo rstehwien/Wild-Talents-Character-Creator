05d650acd27e0ce801763fa402db794cae407319
Robert Stehwien <robert.stehwien@universalmind.com>
2010-02-05 22:54:28 -0700

    Add initial tabs

--- Resources/wtcc/wtcc_app.js
+++ Resources/wtcc/wtcc_app.js
@@ -1,14 +1,30 @@
-$(document).ready(function(){
-	$.ajax({
-		type: "GET",
-		url: "wtcc/xml/WildTalentsCharacterCreatorData.xml",
-		dataType: "xml",
-		success: function(xml) {
-			$(xml).find('config-choice').each(function(){
-				var type = $(this).attr('type');
-				var link = $(this).attr('link');
-				$('<div class="items"></div>').html('<p>'+type+': '+link+'</a>').appendTo('#page-wrap');
-			});
-		}
-	});
+var WTCC = {
+
+    loadConfig: function(xml) {
+        $(xml).find('config-choice').each(function() {
+            var type = $(this).attr('type');
+            var link = $(this).attr('link');
+            $('<div class="items"></div>').html('<p>' + type + ': ' + link + '</p>').appendTo('#wtcc_tab_messages');
+        });
+        // TODO data validation
+        WTCC.xml = xml;
+        WTCC.initPage();
+    },
+
+    documentReady: function() {
+        $.ajax({
+            type: "GET",
+            url: "wtcc/xml/WildTalentsCharacterCreatorData.xml",
+            dataType: "xml",
+            success: WTCC.loadConfig
+        });
+    },
+
+    initPage: function() {
+        $('<div class="items"></div>').html('<p>hello</p>').appendTo('#wtcc_tab_info');
+    },
+};
+
+$(function() {
+    WTCC.documentReady();
 });
