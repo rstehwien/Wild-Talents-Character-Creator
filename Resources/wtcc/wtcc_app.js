754dcf9000ba7c5c8514988f77128782d786ecf5
Robert Stehwien <robert.stehwien@universalmind.com>
2010-02-06 07:59:26 -0700

    add text areas for xml

--- Resources/wtcc/wtcc_app.js
+++ Resources/wtcc/wtcc_app.js
@@ -1,19 +1,25 @@
 var WTCC = {
 
     loadDataXml: function(xml) {
-        $(xml).find('config-choice').each(function() {
+        WTCC.config = xml;
+        WTCC.jconfig = $(xml);
+        WTCC.jconfig.find('config-choice').each(function() {
             var type = $(this).attr('type');
             var link = $(this).attr('link');
-            $('<div class="items"></div>').html('<p>' + type + ': ' + link + '</p>').appendTo('#wtcc_tab_messages');
+            WTCC.appendMessage(type + ': ' + link);
         });
         // TODO data validation
-        WTCC.xml = xml;
+
         WTCC.initPage();
     },
     
-    loadCharacterXml: function(xml) {
-      
+    appendMessage: function(msg) {
+      $('<p>' + msg + '</p>').appendTo('#wtcc_tab_messages');
     },
+
+    loadCharacterXml: function(xml) {
+
+        },
 
     documentReady: function() {
         $.ajax({
@@ -25,9 +31,31 @@ var WTCC = {
     },
 
     initPage: function() {
-        $('<div class="items"></div>').html('<p>hello</p>').appendTo('#wtcc_tab_info');
+        $('<p>hello</p>').appendTo('#wtcc_tab_info');
+        $('#wtcc_config_textarea').text(WTCC.serializeXML(WTCC.config));
     },
-};
+
+    serializeXML: function(node) {
+        if (typeof XMLSerializer != "undefined") return (new XMLSerializer()).serializeToString(node);
+        else if (node.xml) return node.xml;
+        else throw "XML.serialize is not supported or can't serialize " + node;
+    },
+
+    createXMLDocument: function(string)
+    {
+        var browserName = navigator.appName;
+        var doc;
+        if (browserName == 'Microsoft Internet Explorer')
+        {
+            doc = new ActiveXObject('Microsoft.XMLDOM');
+            doc.async = 'false'
+            doc.loadXML(string);
+        } else {
+            doc = (new DOMParser()).parseFromString(string, 'text/xml');
+        }
+        return doc;
+    },
+}
 
 $(function() {
     WTCC.documentReady();
